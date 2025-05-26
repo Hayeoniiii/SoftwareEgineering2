e3d8561e8afd88ab7c6da7d09daa2070303c9519
김하연 <hayeon2461@gmail.com>
2025-05-27 04:26:56 +0900

    최종

--- source_code/Bike.cpp
+++ source_code/Bike.cpp
@@ -1,10 +1,43 @@
 #include "Bike.h"
-using namespace std;
 
-Bike::Bike(const string& id, const string& maker)
-  : bikeID(id), bikeMaker(maker), rented(false) {}
+std::vector<Bike*> Bike::bikes;
 
-string Bike::getID() const     { return bikeID; }
-string Bike::getMaker() const  { return bikeMaker; }
-bool   Bike::isRented() const  { return rented; }
-void   Bike::setRented(bool r) { rented = r; }
+Bike::Bike(const std::string& id, const std::string& name)
+	: bikeID(id), bikeName(name),isRented(false) {
+}
+
+bool Bike::AddBike(const std::string& id, const std::string& name) {					 
+	bikes.push_back(new Bike(id, name));
+	return true;
+}
+
+const std::vector<Bike*>& Bike::GetBikes() {
+	return bikes;
+}
+
+Bike* Bike::FindBike(const std::string& id) {
+	for (auto b : bikes) {
+		if (b->bikeID == id)
+			return b;
+	}
+	//return nullptr;
+}
+
+bool Bike::UpdateToRent(const std::string& id) {
+	Bike* b = FindBike(id);
+	//if (!b || b->isRented) return false;
+	b->isRented = true;
+	return true;
+}
+
+const std::string& Bike::GetBikeID() const {
+	return bikeID;
+}
+
+const std::string& Bike::GetBikeName() const {
+	return bikeName;
+}
+
+bool Bike::IsRented() const {
+	return isRented;
+}
